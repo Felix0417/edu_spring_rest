19b038c7adba2a6b3ca69237ea91542e5fe7b262
felix <Felix0417@github.com>
2022-06-07 22:26:55 +0300

    Added annotation Mapping method on MyRESTController

--- src/main/java/com/felix/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/felix/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,23 @@
 package com.felix.spring.rest.controller;
 
+import com.felix.spring.rest.entity.Employee;
 import com.felix.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class MyRESTController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
 }
